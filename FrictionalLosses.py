f60e63ab1ba8445e203a613bd5fbf68d305be044
Brongorg <brennan1207@gmail.com>
2023-04-25 21:31:03 -0500

    Update 0.1.3
    Added All Minor Losses from Table1

--- FrictionalLosses.py
+++ FrictionalLosses.py
@@ -1,31 +1,66 @@
 def ReturnMinorLoss(input):
     
     if(input == 'A'): #90 degrees elbow Bend
-        return 1
+        print("Flanged = 0", '\n', "Threaded = 1")
+        select = int(input())
+        if(select == 0):
+            return(0.3)
+        elif(select == 1):
+            return(0.9)
+        else:
+            return(-1)
     
     elif(input == "B"): #90 Degrees Miter Bend
-        return 1
-    
+        print("Vaneless = 0", '\n', "Vanned = 1")
+        select = int(input())
+        if(select == 0):
+            return(1.1)
+        elif(select == 1):
+            return(0.2)
+        else:
+            return(-1)
+
     elif(input == "C"): #45 Degrees threaded Elbow Bend
-        return 1
+        return(0.4)
     
     elif(input == "D"): #180 Degrees Return Bend
-        return 1
+        print("Flanged = 0", '\n', "Threaded = 1")
+        select = int(input())
+        if(select == 0):
+            return(0.1)
+        elif(select == 1):
+            return(1.5)
+        else:
+            return(-1)
     
     elif(input == "E"): #Tee Branch Flow
-        return 1
+        print("Flanged = 0", '\n', "Threaded = 1")
+        select = int(input())
+        if(select == 0):
+            return(1.0)
+        elif(select == 1):
+            return(2.0)
+        else:
+            return(-1)
     
     elif(input == "F"): #Tee Line Flow
-        return 1
+        print("Flanged = 0", '\n', "Threaded = 1")
+        select = int(input())
+        if(select == 0):
+            return(0.2)
+        elif(select == 1):
+            return(0.9)
+        else:
+            return(-1)
     
     elif(input == "G"): #Threaded Union
-        return 1
+        return (0.08)
     
     elif(input == "H"):
         return 1
     
     else:
-        print("input invalid")
+        return -1
     
         
     
