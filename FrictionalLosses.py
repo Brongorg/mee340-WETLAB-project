af844914265006551a9bc52b81050957a4102cfb
Brongorg <brennan1207@gmail.com>
2023-04-25 21:02:46 -0500

    Update 0.1.0
    Added a switch case statement for adding minor losses

---
+++ FrictionalLosses.py
@@ -0,0 +1,32 @@
+def ReturnMinorLoss(input):
+    
+    if(input == 'A'): #90 degrees elbow Bend
+        return 1
+    
+    elif(input == "B"): #90 Degrees Miter Bend
+        return 1
+    
+    elif(input == "C"): #45 Degrees threaded Elbow Bend
+        return 1
+    
+    elif(input == "D"): #180 Degrees Return Bend
+        return 1
+    
+    elif(input == "E"): #Tee Branch Flow
+        return 1
+    
+    elif(input == "F"): #Tee Line Flow
+        return 1
+    
+    elif(input == "G"): #Threaded Union
+        return 1
+    
+    elif(input == "H"):
+        return 1
+    
+    else:
+        print("input invalid")
+    
+        
+    
+    
